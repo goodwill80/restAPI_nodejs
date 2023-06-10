322c9ec3a943074c1cb1d95be7948619df601127
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2023-06-10 22:11:49 +0800

    setup initial route

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const dotenv = require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 6000;
 
+const contactRoutes = require('./routes/contactRoutes');
+
+app.use(contactRoutes);
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
