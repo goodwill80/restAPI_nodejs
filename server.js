21e654580350961958f116b9bb8a6135160a4a08
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2023-06-10 21:29:38 +0800

    setup server

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log('Testing');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 6000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
