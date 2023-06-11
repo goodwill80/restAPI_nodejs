d5ac43d3e8c21dc541d762c2bb0492778ae61a6b
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2023-06-11 10:35:23 +0800

    added error handling middleware

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const app = express();
 const port = process.env.PORT || 6000;
 
 const contactRoutes = require('./routes/contactRoutes');
+const errorHandler = require('./middleware/errorHandler');
 
+// Similar to bodyparser middleware
+app.use(express.json());
+// Error handling middleware
+app.use(errorHandler);
+// Routes middleware
 app.use('/api/contacts', contactRoutes);
 
 app.listen(port, () => {
